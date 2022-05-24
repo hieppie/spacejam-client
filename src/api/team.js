ba51010ca665ecc53e31107cc058ac840c4807e1
hieppie <hiepqduong@gmail.com>
2022-05-23 20:57:19 -0400

    added team api

---
+++ src/api/team.js
@@ -0,0 +1,50 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const createTeam = (team, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + '/teams/',
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    },
+    data: {
+      team: {
+        name: team.name
+      }
+    }
+  })
+}
+
+export const showTeam = (user, id) => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/teams/' + id,
+    headers: {
+      Authoriation: `Bearer ${user.token}`
+    }
+  })
+}
+
+export const updateTeam = (team, user, id) => {
+  return axios({
+    method: 'PATCH',
+    url: apiUrl + '/surveys' + id,
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    },
+    data: {
+      team: team.name
+    }
+  })
+}
+
+export const deleteTeam = (user, id) => {
+  return axios({
+    method: 'DELETE',
+    url: apiUrl + '/teams/' + id,
+    headers: {
+      Authoriation: `Bearer ${user.token}`
+    }
+  })
+}
---
+++ src/components/Team/CreateTeam.js
@@ -0,0 +1,3 @@
+import Reac, {Component} from 'react'
+import { withRouter } from 'react-router-dom'
+import { createTeam } '.'
